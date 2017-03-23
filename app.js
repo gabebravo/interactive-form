56ac990f4f761dece8b4386615db89799a8a3fe4
Gabe Bravo <gabecbravo@gmail.com>
2017-03-23 14:07:03 -0400

    Changed linking of JS files.

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-function loadScripts(){
-   let directory = 'js/';
-   let extension = '.js';
-   let files = ['job-role', 'tshirt-info', 'register-activities', 'payment-info'];
-   for (let file of files){
-       let path = (`${directory}${file}${extension}`);
-       $('body').append(`<script type="text/javascript" src="${path}"></script>`);
-   }
+// function loadScripts(){
+//    let directory = 'js/';
+//    let extension = '.js';
+//    let files = ['job-role', 'tshirt-info', 'register-activities', 'payment-info'];
+//    for (let file of files){
+//        let path = (`${directory}${file}${extension}`);
+//        $('body').append(`<script type="text/javascript" src="${path}"></script>`);
+//    }
+// }
+//loadScripts();
+
+$( window ).on( "load", pageloadHandler );
+
+function pageloadHandler(evt){
+   $('input#name').focus();
 }
 
-loadScripts();
-
-jobRole.getGreeting();
-shirtInfo.getGreeting();
-activities.getGreeting();
-paymetInfo.getGreeting();
+$(document).ready(function() {
+   jobRole.getGreeting();
+   shirtInfo.getGreeting();
+   activities.getGreeting();
+   paymetInfo.getGreeting();
+});
